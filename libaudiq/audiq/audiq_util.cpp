0c65d9145d0373d5f50e3246847a1f30f6fb8d40
SupremaLex <georglutstnko@gmail.com>
2018-04-09 13:51:42 +0300

    Add new support functions. Move GenerateResultFile to other namespace

---
+++ libaudiq/audiq/audiq_util.cpp
@@ -0,0 +1,193 @@
+#include "audiq/audiq_util.h"
+#include <map>
+#include <experimental/filesystem>
+#include "gaia2/utils.h"
+#include "audiq/audiq_config.h"
+
+namespace audiq {
+namespace util {
+
+using gaia2::ParameterMap;
+namespace filesystem = std::experimental::filesystem;
+
+void ConcatenateDataSets(const string &datasets_directory, const string &dataset_name) {
+  DataSet ds;
+  DataSet* result = &ds;
+  bool first = true;
+  for ( auto& p : filesystem::recursive_directory_iterator(datasets_directory) ) {
+    if ( first ) {
+      result->load(QString::fromStdString(p.path().string()));
+      first = false;
+      continue;
+    }
+    DataSet loaded;
+    loaded.load(QString::fromStdString(p.path().string()));
+    result->appendDataSet(&loaded);
+  }
+  if ( !result->pointNames().empty() )
+    result->save(QString::fromStdString(dataset_name + ".db"));
+}
+
+void MergeFiles(const string &files_directory, const string &datasets_directory,
+                const string &dataset_name, const int n) {
+  ReCreateDirs(datasets_directory);
+  string name;
+  string type;
+  map<string, QVector<Point*> > samples;
+  map<string, int> counters;
+  for ( auto t : types::TYPES ) {
+    samples[t] = QVector<Point*>();
+    counters[t] = 0;
+  }
+  for ( auto& p : filesystem::recursive_directory_iterator(files_directory) ) {
+    if ( p.path().extension() != ".sig" ) {
+      continue;
+    }
+    for ( auto& pair : counters ) {
+      if ( pair.second % n == 0 && !samples[pair.first].empty() ) {
+        name = datasets_directory + pair.first + "/" + pair.first + "_"
+             + dataset_name + std::to_string(pair.second / n) + ".db";
+        SaveDataSet(samples[pair.first], name);
+        for ( auto s : samples[pair.first] )
+          delete s;
+        samples[pair.first].clear();
+        }
+    }
+    Point* sample = LoadPoint(p.path().string(), p.path().filename().string());
+    type = sample->label(TYPE_DESCRIPTOR).toSingleValue().toStdString();
+    samples[type] << sample;
+    ++counters[type];
+  }
+  for ( auto pair : samples ) {
+    if ( !pair.second.empty() ) {
+      name = datasets_directory + "/" + pair.first + "/" + pair.first + "_"
+           + dataset_name + std::to_string(pair.second.size() / n + 1) + ".db";
+      SaveDataSet(pair.second, name);
+      for ( auto s : pair.second )
+        delete s;
+      pair.second.clear();
+      }
+  }
+}
+
+void SaveDataSet(const QVector<Point*> &samples, const string &dataset_name) {
+  DataSet dataset;
+  dataset.addPoints(samples);
+  // Some transformations to reduce memory usage in future
+  DataSet* remove_vl = gaia2::transform(&dataset, "RemoveVL");
+  DataSet* transformed = gaia2::transform(remove_vl, "FixLength");
+  transformed->setName(QString::fromStdString(dataset_name));
+  transformed->save(QString::fromStdString(dataset_name));
+  delete remove_vl;
+  delete transformed;
+}
+
+void ReCreateDirs(const string &root_directory) {
+  for ( auto t : types::TYPES ) {
+    if ( filesystem::exists(filesystem::path(root_directory + "/" + t)) )
+      filesystem::remove_all(root_directory + "/" + t);
+    }
+  if ( !filesystem::exists(filesystem::path(root_directory)) )
+    filesystem::create_directory(root_directory);
+  for ( auto t : types::TYPES ) {
+      filesystem::create_directory(root_directory + "/" + t);
+    }
+}
+
+Point* LoadPoint(const string &file_name, const string &point_name) {
+  Point* point = new Point;
+  point->load(QString::fromStdString(file_name));
+  point->setName(QString::fromStdString(point_name));
+  /* different codecs have different number and types of additional information, such tags,
+  so we will only take file_name as tags. Because of QRegex Wildcard's implementation, we can't use [!f]...
+  QStringList tags = { ".metadata.tags.[qwertyuiopasdghjklzxcvbnm]*" };
+  if ( !point->layout().descriptorNames(gaia2::StringType, tags, false).empty() ) {
+    point->layout().filter(QStringList() << "*", tags);
+    point->setLayout(point->layout());
+  }*/
+  return point;
+}
+
+DataSet* MergeDataSets(const vector<DataSet*> &datasets) {
+  DataSet* result = datasets[0];
+  for ( int i = 1; i < static_cast<int>(datasets.size()); ++i ) {
+    result = gaia2::mergeDataSets(result, datasets[i]);
+  }
+  return result;
+}
+
+DataSet* SumDataSets(const vector<DataSet*> &datasets) {
+  DataSet* previous = datasets[0];
+  DataSet* result;
+  for ( int i = 1; i < static_cast<int>(datasets.size()); ++i ) {
+    result = SumDataSets(previous, datasets[i]);
+    if ( i > 1)
+      delete previous;
+    previous = result;
+  }
+  return result;
+}
+
+DataSet* SumDataSets(DataSet *first, DataSet *second) {
+  QStringList common_descriptors = Intersection(first->layout().descriptorNames(),
+                                                second->layout().descriptorNames());
+  QStringList points_intersection = Intersection(first->pointNames(),
+                                                 second->pointNames());
+  ParameterMap select_params;
+  select_params.insert("descriptorNames", common_descriptors);
+  DataSet* selected_first = first;
+  DataSet* selected_second = second;
+  // first of all - delete intersection of points from one of datasets
+  selected_second->removePoints(points_intersection);
+  bool free = false;
+  if ( first->layout().descriptorNames() != common_descriptors ) {
+    selected_first  = gaia2::transform(first, "Select", select_params);
+  }
+  if ( first->layout().descriptorNames() != common_descriptors ) {
+    selected_second = gaia2::transform(second, "Select", select_params);
+    free = true;
+  }
+  selected_first->forgetHistory();
+  selected_second->forgetHistory();
+  selected_first->appendDataSet(selected_second);
+  if ( free ) {
+    delete selected_second;
+  }
+  return selected_first;
+}
+
+QStringList Intersection(const vector<DataSet*> &datasets, int type) {
+  vector<QStringList> data;
+  for ( auto d : datasets ) {
+    if ( type == TYPE_DESCRIPTORS ) {
+      data.push_back(d->layout().descriptorNames());
+    } else if ( type == TYPE_POINTS ) {
+            data.push_back(d->pointNames());
+    }
+  }
+  return Intersection(data);
+}
+
+QStringList Intersection(const vector<QStringList> &lists) {
+  QStringList result = lists[0];
+  for ( int i = 1; i < static_cast<int>(lists.size()); ++i ) {
+    result = Intersection(result, lists[i]);
+  }
+  return result;
+}
+
+QStringList Intersection(const QStringList &first, const QStringList &second) {
+  return first.toSet().intersect(second.toSet()).toList();
+}
+
+string replaceStrChar(string str, const string& replace, char ch) {
+  size_t found = str.find_first_of(replace);
+  while ( found != string::npos ) {
+    str[found] = ch;
+    found = str.find_first_of(replace, found+1);
+  }
+  return str;
+}
+
+}  // namespace util
+}  // namespace audiq
---
+++ libaudiq/audiq/audiq_util.h
@@ -0,0 +1,86 @@
+#ifndef PROJECT_AUDIQ_AUDIQ_UTIL_H
+#define PROJECT_AUDIQ_AUDIQ_UTIL_H
+
+#include "audiq/audiq_util.h"
+#include <string>
+#include <vector>
+#include <QVector>
+#include <QStringList>
+#include "gaia2/point.h"
+#include "gaia2/dataset.h"
+#include "audiq/audiq_types.h"
+#include "audiq/audiq_config.h"
+
+#define TYPE_DESCRIPTORS 1
+#define TYPE_POINTS 2
+
+namespace audiq {
+namespace util {
+
+using gaia2::Point;
+using gaia2::DataSet;
+
+/**
+ * @brief ConcatenateDataSets Concatenate datasets from 'datasets_directory' and save result dataset as 'dataset_name'.
+ * @param datasets_directory Directory with datasets.
+ * @param dataset_name Result dataset name.
+ */
+void ConcatenateDataSets(const string &datasets_directory = DATASETS_DIR,
+                         const string &dataset_name = USER_DATASET_NAME);
+/**
+ * @brief MergeFiles Merges files with descriptors to datasets. Result of function - directory with datasets.
+ *  Each dataset has name 'dataset_name' + number, and don't have more them 'samples_per_dataset' samples in it.
+ * @param files_directory Directory with descriptors.
+ * @param datasets_directory Directory with datasets parts.
+ * @param dataset_name Name of the created dataset.
+ * @param samples_per_dataset Number of samples in dataset chunk.
+ */
+void MergeFiles(const string &files_directory = DESCRIPTORS_DIR,
+                const string &datasets_directory = DATASETS_DIR,
+                const string &dataset_name = USER_DATASET_PART,
+                const int samples_per_dataset = SAMPLES_PER_DATASET);
+/**
+ * SaveDataSetPart Creates dataset from 'samples' and save it as 'dataset_name'.
+ */
+void SaveDataSet(const QVector<Point*> &samples, const string &dataset_name);
+
+void ReCreateDirs(const string &root_directory);
+
+/**
+ * LoadPoint Loads point from yaml file. If you don't need loaded point - free memory.
+ */
+Point* LoadPoint(const string &file_name, const string &point_name);
+/**
+ * MergeDataSets Merges datasets together, provided that their layout don't overlap, and
+ * return the resulting dataset.
+ */
+DataSet* MergeDataSets(const vector<DataSet*> &datasets);
+/**
+ * SumDataSets Sums datasets. The result dataset contains only intersection of there descriptors.
+ */
+DataSet* SumDataSets(const vector<DataSet*> &datasets);
+/**
+ * SumDataSets Sums two datasets. The result dataset contains only intersection of there descriptors.
+ */
+DataSet* SumDataSets(DataSet *first, DataSet *second);
+/**
+ * Intersection Find intersection of 'datasets' descriptors or point's (this depends on 'type')
+ */
+QStringList Intersection(const vector<DataSet*> &datasets, int type);
+/**
+ * Intersection Finds intersection of 'lists'.
+ */
+QStringList Intersection(const vector<QStringList> &lists);
+/**
+ * Intersection Finds intersection of two lists.
+ */
+QStringList Intersection(const QStringList &first, const QStringList &second);
+
+/**
+  Replace char 'ch' in str.
+ */
+string replaceStrChar(string str, const string& replace, char ch);
+
+}  // namespace util
+}  // namespace audiq
+#endif  // PROJECT_AUDIQ_AUDIQ_UTIL_H
